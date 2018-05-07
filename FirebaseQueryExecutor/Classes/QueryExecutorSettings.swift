7ed72925ec6d7f9a01fb1c77df6824dc377a3202
Pavel Mosunov <pavel.mosunov@anoda.mobi>
2018-05-07 12:29:47 +0300

    public properties + typealias

--- FirebaseQueryExecutor/Classes/QueryExecutorSettings.swift
+++ FirebaseQueryExecutor/Classes/QueryExecutorSettings.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 //NOTE: to simplify calls
-typealias ExecutorSettings = QueryExecutorSettings
+public typealias ExecutorSettings = QueryExecutorSettings
 
 public class QueryExecutorSettings {
-    static let shared: QueryExecutorSettings = QueryExecutorSettings()
+    public static let shared: QueryExecutorSettings = QueryExecutorSettings()
     
     //NOTE: Set this to false to reload all documents on doc changes. Call this on app start.
-    var shouldObserveOnlyDiffs: Bool = true
+    public var shouldObserveOnlyDiffs: Bool = true
 }
