e87909d00a90bb8aafb02e25043d36df0f4d3605
Pavel Mosunov <pavel.mosunov@anoda.mobi>
2018-05-07 12:27:29 +0300

    settings become public

--- FirebaseQueryExecutor/Classes/QueryExecutorSettings.swift
+++ FirebaseQueryExecutor/Classes/QueryExecutorSettings.swift
@@ -10,7 +10,7 @@ import Foundation
 //NOTE: to simplify calls
 typealias ExecutorSettings = QueryExecutorSettings
 
-class QueryExecutorSettings {
+public class QueryExecutorSettings {
     static let shared: QueryExecutorSettings = QueryExecutorSettings()
     
     //NOTE: Set this to false to reload all documents on doc changes. Call this on app start.
