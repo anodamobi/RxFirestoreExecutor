5d98526d6cc9997445a01bdd4d0e88dee76c18c2
Pavel Mosunov <pavel.mosunov@anoda.mobi>
2018-10-03 13:55:37 +0300

    Object type for RxObject changed to RxObject

--- FirebaseQueryExecutor/Classes/Base/RxObject.swift
+++ FirebaseQueryExecutor/Classes/Base/RxObject.swift
@@ -9,17 +9,18 @@ import Foundation
 import RxSwift
 
 open class RxObject: FEObject, SelfExecutable {
+    
     public typealias ObjectType = RxObject
-        
-    open func pull() -> Single<ObjectType> {
+    
+    open func pull() -> Single<RxObject> {
         return super.pull()
     }
-    
-    open func push(_ object: ObjectType) -> Single<ObjectType> {
+
+    open func push(_ object: RxObject) -> Single<RxObject> {
         return super.push(object)
     }
-    
-    open func observe() -> Observable<ObjectType> {
+
+    open func observe() -> Observable<RxObject> {
         return super.observe()
     }
 }
